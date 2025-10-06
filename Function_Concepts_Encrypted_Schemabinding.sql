739a7ea4e059445301540534bfe8119af3fe8f5b
Mark Jürgen <markjurgennn@gmail.com>
2025-10-06 09:54:05 +0300

    Funktsioon seoti tabeliga SCHEMABINDING abil

--- Function_Concepts_Encrypted_Schemabinding.sql
+++ Function_Concepts_Encrypted_Schemabinding.sql
@@ -14,3 +14,19 @@ END
 
 EXEC sp_helptext fn_GetEmployeeNameById;
 
+DROP FUNCTION fn_GetEmployeeNameById;
+
+
+CREATE FUNCTION fn_GetEmployeeNameById(@Id INT)
+RETURNS NVARCHAR(100)
+WITH SCHEMABINDING
+AS
+BEGIN
+    DECLARE @Name NVARCHAR(100)
+    SELECT @Name = FirstName + ' ' + LastName FROM dbo.DimEmployee WHERE EmployeeKey = @Id
+    RETURN @Name
+END
+
+
+
+
